fdaf8352fe58261b7cc4e989ffdab4ca3fb68a09
bruno <brunoliracz@gmail.com>
2020-08-11 14:52:52 -0300

    criando projeto

---
+++ src/main/java/com/br/springawsuploadimage/SpringAwsUploadImageApplication.java
@@ -0,0 +1,13 @@
+package com.br.springawsuploadimage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAwsUploadImageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAwsUploadImageApplication.class, args);
+    }
+
+}
